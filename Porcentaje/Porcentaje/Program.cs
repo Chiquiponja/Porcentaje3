e51a32c6370a7401a45ee80160c50e01566692af
Chiquiponja <46056373+Chiquiponja@users.noreply.github.com>
2019-03-14 12:31:40 -0300

    Add files via upload

---
+++ Porcentaje/Porcentaje/Program.cs
@@ -0,0 +1,45 @@
+﻿using Porcentaje;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglaDeIntereses
+{
+    class EjercicioComprobante
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+            Comprobante.Comprobante comp1 = new Comprobante.Comprobante();
+            comp1.TipoDeComprobante = ("Factura A");
+           
+            
+            comp1.FechaEm = new DateTime(2019, 2, 5);
+            Console.WriteLine("INGRESE NOMBRE");
+            comp1.DatosClientes = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("INGRESE LA FECHA DE PAGO");
+            comp1.FechaVen = Convert.ToDateTime(Console.ReadLine());
+            
+            Console.WriteLine("INGRESE LA FECHA DE VENCIMIENTO");
+
+            comp1.FechaDePago = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("INGRESE EL IMPORTE DE LA FACTURA ");
+            comp1.Importe = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("valor de comprobante es: " + comp1.Importe);
+
+            var recargoService = new RegladeRecargoService();
+            var recargoCobrar = recargoService.Recargo(comp1);
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Porcentaje/Porcentaje/RegladeRecargoService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porcentaje
+{
+    public class RegladeRecargoService
+    {
+
+        
+           
+          
+            public decimal Recargo(Comprobante.Comprobante comprobante)
+            {
+                decimal Recargo = 0;
+                decimal iva = 0;
+                
+               var c = (comprobante.FechaVen - comprobante.FechaDePago).Days;
+
+
+
+            if (c > 0 && c <= 15)
+                {
+                    iva = ((comprobante.Importe) * Convert.ToDecimal(0.005));
+                    Console.WriteLine("tienes  de Pago ");
+                }
+                else if (c > 15 && c <= 30)
+                {
+                    iva = ((comprobante.Importe) * Convert.ToDecimal (0.015));
+                    Console.WriteLine("tienes  de Pago");
+                }
+                else if  (c > 30 && c <= 60)
+                {
+                    iva = ((comprobante.Importe) * Convert.ToDecimal(0.030));
+                    Console.WriteLine("Tiene  de Pago");
+                }
+                else if (c >= 60)
+                {
+                iva = ((comprobante.Importe) * Convert.ToDecimal(0.060));
+                Console.WriteLine("Tiene  de Pago");
+                 }
+            else 
+                {
+                    iva = (comprobante.DatosClientes);
+                    Console.WriteLine("No hay Iva");
+                }
+                
+                Console.Clear();
+                Console.WriteLine("Nombre" + (comprobante.DatosClientes));
+                Console.WriteLine("Valor de la Factura: " + comprobante.Importe);
+                
+                Console.WriteLine("Interes : " + (iva));
+                Console.WriteLine("Total: " + (comprobante.Importe + (iva)));
+                Console.WriteLine();
+                Console.ReadLine();
+                return Recargo;
+            }
+                
+       
+
+
+
+
+            
+                
+            }
+           
+        }
+
+        
+   
+
